a85e70631bef9bb085a6df458efca9e4f5f9364b
mitch <mitchellkurtzman@gmail.com>
2022-02-06 19:45:33 -0500

    hashmap

---
+++ romantointeger.js
@@ -0,0 +1,22 @@
+let roman = new Map([
+    [1, "I"],
+    ["II", 2],
+    ["III", 3],
+    ["IV", 4],
+    ["V", 5],
+    ["IX", 9],
+    ["X", 10],
+    ["XL", 40],
+    ["L", 50],
+    ["LC", 90],
+    ["C", 100],
+    ["CD", 400],
+    ["D", 500],
+    ["DM", 900],
+    ["M", 1000]
+])
+
+console.log(roman.get("IV"));
+
+
+//Learning hashmaps
