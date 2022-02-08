fd1d07346092da098983e22375e70ea175e3f101
mitch <mitchellkurtzman@gmail.com>
2022-02-08 08:32:18 -0500

    started map and loop

--- longestcommonprefix.js
+++ longestcommonprefix.js
@@ -1,8 +1,25 @@
-/**
- * @param {string[]} strs
- * @return {string}
- */
-var longestCommonPrefix = function(strs) {
+strs = ["flower", "flow", "flight"]
 
 
-};
+
+let strindex = 0
+
+
+for (let i = 0; i < strs.length; i++) {
+
+    console.log(strs[i])
+
+    for (let j = 0; j < strs[i].length; j++)[
+        console.log(strs[i][j])
+    ]
+
+
+}
+
+
+
+//split array into 'string x's'
+//split strings into chars?
+//if char[x] = char, compare to x char ... ect
+// push to map or array if all equal
+//return array in string form
